90934f6c532b3db1842b04e3f226423bdef9d602
ciao <gay>
2026-07-22 11:20:00 +0200

    Implement CLI for dataset code generation and refactor dataset download structure

--- src/cli.py
+++ src/cli.py
@@ -1,5 +1,5 @@
 import click
-from utils.io import load_ruamel
+from src.utils.io import load_ruamel
 
 @click.group()
 def main():
@@ -10,5 +10,5 @@ def main():
 @click.option("--parameters", help="Path to the parameters file", required=True)
 @click.option("--hetero", is_flag=True, help="Optional flag to impose heterogeneous graph filtering")
 def dataset_code(parameters, hetero):
-    from dataset_download import generate_code
+    from src.dataset_download import generate_code
     return generate_code(load_ruamel(parameters), hetero)
