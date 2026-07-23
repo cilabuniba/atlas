cfef2a0b9adf0d9a0ee5a5d340f106dc567ab185
ciao <gay>
2026-07-23 15:29:05 +0200

    Fix path separator in Planetoid config and add Movielens explainer run class

---
+++ src/explain/movielens_explainer_run.py
@@ -0,0 +1,10 @@
+from .explainer_run import ExplainerRun
+from src.utils import ParameterKeys
+import torch_geometric.datasets as data_pkg
+from torch_geometric.transforms import RandomLinkSplit
+import src.models as model_pkg
+import torch
+
+
+class MovielensExplainerRun(ExplainerRun):
+    pass
