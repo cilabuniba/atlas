85bbd6a716e4295f7cc7bda216d7670363387754
ciao <gay>
2026-08-26 16:15:13 +0200

    Add configuration files for Planetoid dataset with varying node counts and update dataset generation script

---
+++ scripts/metrics/entire_dataset_gen.bat
@@ -0,0 +1,7 @@
+uv run script_exe.py dataset_code --parameters configs\metrics\dataset_download\planetoid\100.yaml
+uv run script_exe.py dataset_code --parameters configs\metrics\dataset_download\planetoid\500.yaml
+uv run script_exe.py dataset_code --parameters configs\metrics\dataset_download\planetoid\1000.yaml
+uv run script_exe.py dataset_code --parameters configs\metrics\dataset_download\planetoid\2500.yaml
+uv run script_exe.py dataset_code --parameters configs\metrics\dataset_download\planetoid\5000.yaml
+uv run script_exe.py dataset_code --parameters configs\metrics\dataset_download\planetoid\10000.yaml
+
