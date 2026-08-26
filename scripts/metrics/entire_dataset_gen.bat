26f165bd315f8ea63e429f4ff83a79f51e6bfb99
ciao <gay>
2026-08-26 16:22:52 +0200

    Add configuration files for MovieLens dataset with varying node counts and update dataset generation script

--- scripts/metrics/entire_dataset_gen.bat
+++ scripts/metrics/entire_dataset_gen.bat
@@ -5,3 +5,9 @@ uv run script_exe.py dataset_code --parameters configs\metrics\dataset_download\
 uv run script_exe.py dataset_code --parameters configs\metrics\dataset_download\planetoid\5000.yaml
 uv run script_exe.py dataset_code --parameters configs\metrics\dataset_download\planetoid\10000.yaml
 
+uv run script_exe.py dataset_code --parameters configs\metrics\dataset_download\movielens\100.yaml --hetero
+uv run script_exe.py dataset_code --parameters configs\metrics\dataset_download\movielens\500.yaml --hetero
+uv run script_exe.py dataset_code --parameters configs\metrics\dataset_download\movielens\1000.yaml --hetero
+uv run script_exe.py dataset_code --parameters configs\metrics\dataset_download\movielens\2500.yaml --hetero
+uv run script_exe.py dataset_code --parameters configs\metrics\dataset_download\movielens\5000.yaml --hetero
+uv run script_exe.py dataset_code --parameters configs\metrics\dataset_download\movielens\10000.yaml --hetero
